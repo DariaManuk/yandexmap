1a2885a883f42338ce2c4639116875217b40163d
dasha <dashaman06@yandex.ru>
2022-03-08 15:17:09 +0300

    3.3

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import sys
 import pygame
 import requests
 
-dol = {}
-shir = {}
-for i in range(18, -1, -1):
-    dol[i] = 180 / 2 ** i
-for j in range(18, -1, -1):
-    shir[j] = 360 / 2 ** j
-
 if __name__ == '__main__':
     print('Введите долготу координат')
     a = int(input())
@@ -18,6 +11,7 @@ if __name__ == '__main__':
     print('Введите масштаб карты')
     z = int(input())
     pygame.init()
+    k, m = 360 / 2 ** (z-1), 180 / 2 ** (z)
     screen = pygame.display.set_mode((600, 450))
     fps = 60
     clock = pygame.time.Clock()
@@ -29,16 +23,20 @@ if __name__ == '__main__':
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_PAGEUP and z > 0:
                     z -= 1
+                    k *= 2
+                    m *= 2
                 elif event.key == pygame.K_PAGEDOWN and z < 18:
                     z += 1
-                elif event.key == pygame.K_LEFT and a - dol[z] > -90:
-                    a -= dol[z]
-                elif event.key == pygame.K_RIGHT and a + dol[z] < 90:
-                    a += dol[z]
-                elif event.key == pygame.K_UP and b + shir[z] < 180:
-                    b += shir[z]
-                elif event.key == pygame.K_DOWN and b - shir[z] > -180:
-                    b -= shir[z]
+                    k /= 2
+                    m /= 2
+                elif event.key == pygame.K_LEFT and a - k > -90:
+                    a -= k
+                elif event.key == pygame.K_RIGHT and a + k < 90:
+                    a += k
+                elif event.key == pygame.K_UP and b + m < 180:
+                    b += m
+                elif event.key == pygame.K_DOWN and b - m > -180:
+                    b -= m
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={float(a)},{float(b)}&z={z}&l=map"
         response = requests.get(map_request)
         if not response:
